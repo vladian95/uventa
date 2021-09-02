c2b3ad068ce088cb5cd98c73efe0d0b328ca9e48
vladian95 <kvankvan1995@gmail.com>
2021-09-02 15:20:15 +0300

    added bottom content and icons

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
-$(function(){
+const ratingItemsList = document.querySelectorAll('.icon-star');
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
 
+ratingItemsArray.forEach(item => 
+    item.addEventListener('click', () => {
+        const { itemValue } = item.dataset;
+        item.parentNode.dataset.totalValue = itemValue;
+    })
+);
 
-
-    
-});
